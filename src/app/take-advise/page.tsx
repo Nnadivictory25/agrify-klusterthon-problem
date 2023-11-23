7dc60d2669b549dbdec9896a45bd5fa6e09289c4
Anirudh Singh Bhadauria <108342411+AnirudhSinghBhadauria@users.noreply.github.com>
2023-11-23 23:49:07 +0530

    started integrating openai

--- src/app/take-advise/page.tsx
+++ src/app/take-advise/page.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import Agrify from "@/components/assets/logo/agrify-filled-logo";
+import React from "react";
 
 const Advise = () => {
   return (
-    <div>This is advise page!</div>
-  )
-}
+    <section className="w-full h-screen py-[86px] flex flex-row justify-center">
+      <div className="w-full max-w-[858px] px-8 py-16 flex flex-col items-center">
 
-export default Advise
+        <div className="w-full flex flex-col justify-center items-center mb-[10rem]">
+          <Agrify />
+          <p className="mt-[24px]">How can I help you today?</p>
+        </div>
+
+        <form className="flex flex-row space-x-[16px]">
+          <input className="border-[1px] h-min" type="text" />
+          <button>Send</button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Advise;
---
+++ src/components/assets/logo/agrify-filled-logo.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Agrify = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="72"
+      height="72"
+      viewBox="0 0 72 72"
+      fill="none"
+    >
+      <rect width="72" height="72" rx="18" fill="#050A0A" />
+      <path d="M36 16H26L46 36V26L36 16Z" fill="white" />
+      <path d="M56 16H46V26H56V16Z" fill="white" />
+      <path d="M46 46H36L56 26V36L46 46Z" fill="white" />
+      <path d="M56 46H46V56H56V46Z" fill="white" />
+      <path d="M36 56H46L26 36V46L36 56Z" fill="white" />
+      <path d="M26 46H16V56H26V46Z" fill="white" />
+      <path d="M26 26H36L16 46V36L26 26Z" fill="white" />
+      <path d="M26 16H16V26H26V16Z" fill="white" />
+    </svg>
+  );
+};
+
+export default Agrify;
