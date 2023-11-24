7b8cdbe8e6133764b3d44f90856ed53e33bce332
Anirudh Singh Bhadauria <108342411+AnirudhSinghBhadauria@users.noreply.github.com>
2023-11-24 23:55:02 +0530

    done something right

--- src/app/take-advise/page.tsx
+++ src/app/take-advise/page.tsx
@@ -1,19 +1,23 @@
+'use client'
+
 import Agrify from "@/components/assets/logo/agrify-filled-logo";
+import SendArrow from "@/components/assets/misc/send-arrow";
 import React from "react";
 
 const Advise = () => {
   return (
     <section className="w-full h-screen py-[86px] flex flex-row justify-center">
       <div className="w-full max-w-[858px] px-8 py-16 flex flex-col items-center">
-
         <div className="w-full flex flex-col justify-center items-center mb-[10rem]">
           <Agrify />
           <p className="mt-[24px]">How can I help you today?</p>
         </div>
 
-        <form className="flex flex-row space-x-[16px]">
-          <input className="border-[1px] h-min" type="text" />
-          <button>Send</button>
+        <form className="w-full flex flex-row space-x-[16px] justify-center px-5">
+          <div className="flex flex-row space-x-[16px] border-gray-200 focus:border-gray-400 border-[1px] w-full  pr-[20px] rounded-[20px]">
+            <textarea className="grid place-items-center w-full py-[8px] px-[16px] rounded-[20px] text-[16px] overflow-y-auto chat" placeholder="Ask anything..." rows={1} />
+            <button><SendArrow /></button>
+          </div>
         </form>
       </div>
     </section>
---
+++ src/components/assets/misc/send-arrow.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const SendArrow = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      className="h-5 w-5 text-gray-400 hover:text-gray-800 transition-all duration-200 ease-in-out"
+    >
+      <path d="m3 3 3 9-3 9 19-9Z"></path>
+      <path d="M6 12h16"></path>
+    </svg>
+  );
+};
+
+export default SendArrow;
